47c8e125206be4f5aaeef98b93eadd11128fd65a
Luiz Bills <luizbills@pm.me>
2024-04-16 19:52:25 -0300

    new examples

---
+++ samples/custom-canvas/custom-canvas.js
@@ -0,0 +1,8 @@
+litecanvas({
+  canvas: '#game-canvas',
+})
+
+function draw() {
+  clear(0)
+  print(20, 20, CANVAS.id, 3)
+}
---
+++ samples/multiple-canvas/multiple-canvas.js
@@ -0,0 +1,24 @@
+const loop1 = {
+  draw: function () {
+    game1.clear(2)
+  },
+}
+
+const loop2 = {
+  draw: function () {
+    game2.clear(4)
+  },
+}
+
+const game1 = litecanvas({
+  global: false,
+  width: 256,
+  loop: loop1,
+})
+
+const game2 = litecanvas({
+  global: false,
+  autoscale: false,
+  width: 128,
+  loop: loop2,
+})
