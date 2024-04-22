86ee7091365f7a12c7177a772ced3e774354d76a
Luiz Bills <luizbills@pm.me>
2024-04-21 23:09:57 -0300

    add 3d example

---
+++ samples/3d/3d.js
@@ -0,0 +1,105 @@
+litecanvas()
+
+function init() {
+  size = WIDTH > HEIGHT ? HEIGHT / 4 : WIDTH / 4
+  projection = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ]
+  points = [
+    [-size, -size, -size], // [x, y, z]
+    [size, -size, -size],
+    [size, size, -size],
+    [-size, size, -size],
+    [-size, -size, size],
+    [size, -size, size],
+    [size, size, size],
+    [-size, size, size],
+  ]
+  projected = []
+  angle = 0
+  linestyle(4)
+}
+
+function update(dt) {
+  angle += 0.01
+
+  for (let i = 0; i < points.length; i++) {
+    const pos = point2matrix(...points[i])
+    let rotated = matmul(rotationX(angle), pos)
+    rotated = matmul(rotationY(angle), rotated)
+    rotated = matmul(rotationZ(angle), rotated)
+    const projected2D = matmul(projection, rotated)
+    projected[i] = projected2D
+  }
+}
+
+function draw() {
+  cls(0)
+  print(0, 0, FPS, 4)
+
+  push()
+  transform(WIDTH / 2, HEIGHT / 2)
+  for (let i = 0; i < 4; i++) {
+    connect(i, (i + 1) % 4, projected)
+    connect(i + 4, ((i + 1) % 4) + 4, projected)
+    connect(i, i + 4, projected)
+  }
+  pop()
+}
+
+function connect(a, b, points) {
+  line(points[a][0][0], points[a][1][0], points[b][0][0], points[b][1][0], 3)
+}
+
+function matmul(a, b) {
+  const acols = a[0].length
+  const arows = a.length
+  const bcols = b[0].length
+  const brows = b.length
+
+  if (acols !== brows) throw new Error('Invalid matrixes')
+
+  const res = []
+  for (let i = 0; i < arows; i++) {
+    res[i] = []
+    for (let j = 0; j < bcols; j++) {
+      let sum = 0
+      for (let k = 0; k < acols; k++) {
+        sum += a[i][k] * b[k][j]
+      }
+      res[i][j] = sum
+    }
+  }
+
+  return res
+}
+
+function rotationX(radians) {
+  return [
+    [1, 0, 0],
+    [0, cos(radians), -sin(radians), 0],
+    [0, sin(radians), cos(radians), 0],
+  ]
+}
+
+function rotationY(radians) {
+  return [
+    [cos(radians), 0, sin(radians)],
+    [0, 1, 0],
+    [-sin(radians), 0, cos(radians)],
+  ]
+}
+
+function rotationZ(radians) {
+  return [
+    [cos(radians), -sin(radians), 0],
+    [sin(radians), cos(radians), 0],
+    [0, 0, 1],
+  ]
+}
+
+function point2matrix(x, y, z) {
+  return [[x], [y], [z]]
+}
