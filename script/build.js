28f53c1b71103af09b5df0d37000f2c388304700
Luiz Bills <luizbills@pm.me>
2024-05-07 22:07:20 -0300

    improve build

---
+++ script/build.js
@@ -0,0 +1,32 @@
+import esbuild from 'esbuild'
+import fs from 'node:fs'
+
+let size
+
+await esbuild.build({
+    entryPoints: ['src/index.js'],
+    outfile: 'dist/dist.js',
+    bundle: true,
+    legalComments: 'eof',
+})
+
+size = filesize('dist/dist.js')
+console.log(`  dist/dist.js (${size})`)
+
+await esbuild.build({
+    entryPoints: ['src/index.js'],
+    outfile: 'dist/dist.min.js',
+    bundle: true,
+    minify: true,
+    legalComments: 'eof',
+    sourcemap: true,
+})
+
+size = filesize('dist/dist.min.js')
+console.log(`  dist/dist.min.js (${size})`)
+
+function filesize(filename) {
+    var stats = fs.statSync(filename)
+    var fileSizeInBytes = stats.size
+    return (fileSizeInBytes / 1000).toFixed(1) + 'kb'
+}
