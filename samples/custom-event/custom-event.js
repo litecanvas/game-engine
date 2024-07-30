d9ac1adcc3f1e7206634664196618be00ebc3a8e
Luiz Bills <luizbills@pm.me>
2024-07-30 08:23:05 -0300

    add custom event demo

--- samples/custom-event/custom-event.js
+++ samples/custom-event/custom-event.js
@@ -8,26 +8,25 @@ function init() {
     color = 5
 }
 
-const unlistenThisEvent = listen('player-moved', (player, x, y) => {
-    unlistenThisEvent() // run once
-    if (colrect(...rect, ...player)) {
-        color = 8
-        console.log('collision detected')
-    } else {
-        color = 5
-    }
-})
-
-function tapped(x, y) {
-    player[0] = x
-    player[1] = y
-    emit('player-moved', player, x, y)
-}
-
-function update() {}
-
 function draw() {
     cls(0)
     rectfill(...rect, 4)
     rectfill(...player, color)
 }
+
+function tapped(x, y) {
+    player[0] = x
+    player[1] = y
+    emit('player-moved', player, x, y) // our custom event
+}
+
+const unlistenThisEvent = listen('player-moved', (player, x, y) => {
+    if (colrect(...rect, ...player)) {
+        color = 8
+        console.log('collision detected')
+        // run until collision happens
+        unlistenThisEvent()
+    } else {
+        color = 5
+    }
+})
