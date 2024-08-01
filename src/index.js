4fc4929d263208f5a86b296215f04bc352112f4f
Luiz Bills <luizbills@pm.me>
2024-08-01 08:08:38 -0300

    fix pong demo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* litecanvas v0.41.2 | https://github.com/litecanvas/game-engine */
+/* litecanvas v0.41.3 | https://github.com/litecanvas/game-engine */
 import './zzfx'
 import { colors } from './palette'
 import { sounds } from './sounds'
