ffe28c02f83e952d3953b2c9d63c425c21093842
Luiz Bills <luizbills@pm.me>
2025-01-14 10:17:10 -0300

    add unit test for quit() method

--- tests/_mocks/events.js
+++ tests/_mocks/events.js
@@ -1,5 +1,28 @@
-export function addEventListener(eventName, callback, options = {}) {
-    this._events = this._events || []
-    this._events[eventName] = this._events[eventName] || []
-    this._events[eventName].push({ callback, options })
+export const _listeners = []
+
+/**
+ * @param {string} eventName
+ * @param {Function} callback
+ * @param {boolean|object} options
+ */
+export function addEventListener(eventName, callback, options = false) {
+    _listeners.push([this, eventName, callback, options])
+}
+
+/**
+ * @param {string} eventName
+ * @param {Function} callback
+ * @param {boolean|object} options
+ */
+export function removeEventListener(eventName, callback, options = false) {
+    for (let i = 0; i < _listeners.length; i++) {
+        const args = _listeners[i]
+
+        if (args[0] !== this) continue
+        if (args[1] !== eventName) continue
+        if (args[2] !== callback) continue
+        if (args[3] !== options) continue
+
+        _listeners.splice(i, 1)
+    }
 }
