ca804662b1623889dc20f87b7d4c0cc266b3af12
Luiz Bills <luizbills@pm.me>
2026-06-16 10:51:28 -0300

    v0.302
    * Now `pal()` (without arguments) restore the previous color pallete (not the default).

--- tests/pal.js
+++ tests/pal.js
@@ -22,7 +22,7 @@ test.after(() => {
     local.quit()
 })
 
-test('change the color palette', async (t) => {
+test('change the current color palette', async (t) => {
     await onLitecanvas(local, 'draw', () => {
         const customPalette = ['#123', '#456']
         local.pal(customPalette)
@@ -33,12 +33,20 @@ test('change the color palette', async (t) => {
     })
 })
 
-test('reset the color palette', async (t) => {
+test('restore the previous color palette', async (t) => {
     await onLitecanvas(local, 'draw', () => {
+        const _firstPalette = ['#114', '#fee']
+        local.pal(_firstPalette)
+
+        const _secondPalette = ['#213', '#abc']
+        local.pal(_secondPalette)
+
+        const _currentPalette = local.stat(5)
+        t.not(_currentPalette[0], _firstPalette[0])
+
         local.pal()
+        const _restoredPalette = local.stat(5)
 
-        const colors = local.stat(5)
-
-        t.is(colors[0], defaultPalette[0])
+        t.is(_restoredPalette[0], _firstPalette[0])
     })
 })
